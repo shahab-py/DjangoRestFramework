4944a703f91a2ab5707b22c040737608e11f99fb
shahab <ashahhab2727@gmail.com>
2023-04-02 18:13:52 +0330

    permission

--- home/views.py
+++ home/views.py
@@ -2,9 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import PersonSerializer
 from .models import Person
+from rest_framework.permissions import IsAuthenticated
 
 
 class Home(APIView):
+    permission_classes = [IsAuthenticated,]
     def get(self, request):
         persons = Person.objects.all()
         ser_data = PersonSerializer(instance=persons, many=True)
