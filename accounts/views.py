5cbc9ab2c2b9f6391697d6205188325dcadcff63
shahab <ashahhab2727@gmail.com>
2023-04-02 12:13:25 +0330

    status codes

--- accounts/views.py
+++ accounts/views.py
@@ -3,11 +3,12 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserRegisterSerializer
+from rest_framework import status
 
 class UserRegister(APIView):
     def post(self, request):
         ser_data = UserRegisterSerializer(data=request.POST)
         if ser_data.is_valid():
             ser_data.create(ser_data.validated_data)         
-            return Response(ser_data.data)
-        return Response(ser_data.errors)
+            return Response(ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
